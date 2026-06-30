6fedbd5ea0e2e61fee9bda7d1bf2edbaf4181b43
Maso <mmaso@deloitte.com.pg>
2026-06-30 14:12:44 +1000

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require("express"); 
 const cors = require("cors");
-const resume = require("./data");
-
+const resume = require("./data"); 
+ 
 const app = express();
 app.use(cors());
 
-app.get("/api/resume", (req, res) => {
+app.get("/api/resume", (req, res) => { 
   res.json(resume);
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
